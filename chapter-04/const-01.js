a5fe0318b744972620e670a2238fbc4e341d4f07
hmolhem <hosseinmolhem@gmail.com>
2021-12-03 01:22:35 +0330

    javaScript-code-const-01-version-01

---
+++ chapter-04/const-01.js
@@ -0,0 +1,16 @@
+var x = 10;
+const y =20;
+varStyle();
+
+
+function varStyle(){
+    document.getElementsByClassName('var')[0].innerText=x;
+    document.getElementsByClassName('var')[0].style.fontSize = 'X-large';
+}
+
+
+document.getElementsByClassName('const')[0].innerText=y;
+
+
+// x = 20;
+// y =30;
