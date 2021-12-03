d872b2e016ba436e7e60d4959a15da5665060af3
hmolhem <hosseinmolhem@gmail.com>
2021-12-03 16:20:28 +0330

    javaScript-code-const-version-02

--- chapter-04/const-01.js
+++ chapter-04/const-01.js
@@ -1,16 +1,20 @@
-var x = 10;
+let x = 10;
 const y =20;
-varStyle();
+let variable = document.getElementById('variable');
+let constant = document.getElementById('const');
+let btnSubmit = document.getElementById('submit');
 
+// document.getElementsByClassName('var')[0].style.fontSize = 'Xx-Large';
+// document.getElementsByClassName('const')[0].style.fontSize = 'Xx-Large';
+// document.getElementsByClassName('message')[0].style.fontSize = 'Xx-Large';
 
-function varStyle(){
-    document.getElementsByClassName('var')[0].innerText=x;
-    document.getElementsByClassName('var')[0].style.fontSize = 'X-large';
-}
-
-
-document.getElementsByClassName('const')[0].innerText=y;
-
-
-// x = 20;
-// y =30;
+btnSubmit.onclick = ()=>{
+    
+    try {
+        x = Number(variable.value);
+        y = Number(constant.value);
+        throw "Error occurred";
+    } catch (error) {
+        document.getElementsByClassName('message')[0].innerText = error;
+    }
+};
