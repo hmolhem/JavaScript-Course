53a42c98cc5587c4fca1fd8951168f8a855336d6
hmolhem <hosseinmolhem@gmail.com>
2022-01-24 00:04:06 +0330

    addEventListener for multi object-flashcard project

--- chapter-05/Project-05/app.js
+++ chapter-05/Project-05/app.js
@@ -19,7 +19,7 @@ data = {
 };
 
 const result = document.querySelector('.result');
-const dialog = document.querySelector('.example-dialog');
+const dialog = document.querySelector('.dialog');
 
 dialog.addEventListener('close', (event) => {
     result.textContent = 'dialog was closed';
@@ -62,9 +62,47 @@ var rendered = Mustache.render(template, data);
 document.getElementById('target').innerHTML = rendered;
 
 
-const showAnswer = document.querySelectorAll('#showAnswer');
+// const showAnswer = document.querySelectorAll('#showAnswer');
 // showAnswer.addEventListener('mousemove',showHideAnswer);
 
 function showHideAnswer() {
     window.alert('Hello');
 }
+
+
+let  cardBodyArray = document.querySelectorAll('.card-body');
+
+cardBodyArray.forEach((card,index) => {
+    let  ps = card.querySelectorAll('p');
+    card.addEventListener('click', event =>{
+
+        let  flag = event.target.id;
+        switch (flag) {
+            case "showAnswer":
+                // console.log(`You click on Show Answer with ${index}`);
+                // showHideAnswer(); 
+                if (ps[1].style.visibility === 'hidden'){
+                    ps[1].style.visibility = 'visible';
+                }else{
+                    ps[1].style.visibility = 'hidden';
+                }  
+                break;
+            case "editQuestion":
+                // console.log(`You click on Edit Equation with ${index}`);
+                // alert(ps[0].innerText);
+                // alert(ps[1].innerText);
+                
+                document.getElementById('question').value = ps[0].innerText.slice(2);
+                document.getElementById('answer').value = ps[1].innerText.slice(2);
+                dialog.showModal();
+                // push to array
+
+                break;
+            case "deleteQuestion":
+                console.log(`You click on Delete Equation with ${index}`);
+                break;
+                default:
+                    break;
+                }
+    });
+});
