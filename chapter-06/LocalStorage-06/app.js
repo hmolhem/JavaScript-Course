b96c29cadf4e711aeb3544ca3cdf4792357549c6
hmolhem <hosseinmolhem@gmail.com>
2022-02-05 09:27:38 +0330

    Storing and retrieving JavaScript Array in LOcalStorage - V06

---
+++ chapter-06/LocalStorage-06/app.js
@@ -0,0 +1,54 @@
+// ES6 Class
+
+class toDo {
+    constructor() {
+        this.formTask = document.querySelector('form');
+    }
+    
+    // addTask method
+    addTask(e) {
+        // Add to LocalStorage
+        const task = document.getElementById("task").value;
+        let tasks;
+        const flag = localStorage.getItem('tasks') === null;
+
+        flag ? tasks = [] : tasks = JSON.parse(localStorage.getItem('tasks'));
+
+        tasks.push(task);
+
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+
+        alert('Task saved');
+
+        e.preventDefault();
+    }
+
+    //display LocalStorage
+    display() {
+        const tasks = JSON.parse(localStorage.getItem('tasks'));
+        const flag = localStorage.getItem('tasks') === null;
+        if (!flag) {
+            tasks.forEach(function (task) {
+                console.log(task);
+            });
+        }
+    }
+    // remove douplicate
+    removeDoulicate () {
+        const tasks = JSON.parse(localStorage.getItem('tasks'));
+        const flag = localStorage.getItem('tasks') === null;
+        if (!flag) {
+            let uniqueTasks = [];
+            tasks.forEach((c) => {
+                if (!uniqueTasks.includes(c)) {
+                    uniqueTasks.push(c);
+                }
+                localStorage.setItem('tasks', JSON.stringify(uniqueTasks));
+            });
+        }
+    }
+}
+
+const task = new toDo();
+task.formTask.addEventListener('submit', task.addTask);
+task.display();
